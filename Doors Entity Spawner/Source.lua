c5353fa796a9d69f9177a35a74c4113dda8a9ef4
abhiram555 <abhithedev2019@gmail.com>
2022-11-16 01:00:21 +0530

    Fix Crucifix Entity Despawn thing

--- Doors Entity Spawner/Source.lua
+++ Doors Entity Spawner/Source.lua
@@ -268,6 +268,8 @@ Creator.runEntity = function(entity)
                         Connections[entity.Model].Movement:Disconnect()
                         Connections[entity.Model].Drag:Disconnect()
                         entity.Model:SetAttribute("StopMovement", true)
+                        wait(5)
+                        destroy(entity)
                     end
 
                     -- Killing
